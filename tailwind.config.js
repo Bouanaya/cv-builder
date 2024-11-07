284a047d31d5e77d8df4252d727fdda3d7e89266
Bouanaya <bouanayasoufiane@gmail.com>
2024-11-07 23:55:18 +0100

    add navbar and form1  Personal Information

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{css,js}","./index.html",],
+  content: ["./src/**/*.{css,js}","./index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
